f973fbad59f41e8e5eb733b0a5f717726e66dab2
gersusa <43012424+gersusa@users.noreply.github.com>
2019-04-02 11:07:31 -0500

    Add files via upload

---
+++ MyMatlab1.m
@@ -0,0 +1,33 @@
+function MyMatlab1(InFile1, InFile2, InFile3, InFile4, ...
+                          TimeLimitInSeconds, ScoringMethod, NetworkModel)
+%MYMATLAB1 Version1 Script. This script generates solution1.txt
+%
+% Input Arguments:
+% TimeLimitInSeconds and ScoringMethod are not used yet, the algorithm acts
+% as it would for ScoringMethod = 0.
+% InFile1: CON file
+% InFile2: INL file
+% InFile3: RAW file
+% InFile4: ROP file
+
+
+[mpc,contingencies] = convert2mpc(InFile3,InFile4,...
+                                                   InFile2,InFile1);
+try
+    parpool(32)
+catch M
+    switch M.identifier
+        case 'parallel:cluster:LocalProfileNumWorkersExceeded'
+            parpool(feature('numcores'))
+        case 'parallel:convenience:ConnectionOpen'
+            delete(gcp())
+            parpool(feature('numcores'))
+        otherwise
+            rethrow(M)
+    end
+end
+[mpcOPF, pfs] = solveSCOPF(mpc,contingencies);
+create_solution1(mpcOPF,1)
+create_solution2(pfs,contingencies,1)
+end
+
